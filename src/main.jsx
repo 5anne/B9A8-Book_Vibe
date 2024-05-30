5b619961fe0dfc29cf49e8453ca2598613f00f35
5anne <tahaminaanne@gmail.com>
2024-05-30 12:24:08 +0600

    ninth commit

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,13 @@ import Home from './Components/Home/Home';
 import ListedBooks from './Components/ListedBooks/ListedBooks';
 import BookDetails from './Components/BookDetails/BookDetails';
 import ReadPages from './Components/ReadPages/ReadPages';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/home",
