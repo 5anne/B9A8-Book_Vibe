b499c4fb064ec265d60d0a22c8bcee5aeb7096d1
5anne <tahaminaanne@gmail.com>
2024-05-31 00:10:21 +0600

    eleventh commit

--- src/Components/ListedBooks/ListedBooks.jsx
+++ src/Components/ListedBooks/ListedBooks.jsx
@@ -1,36 +1,12 @@
-import { useLoaderData } from 'react-router-dom';
+
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import { getStoredBooks } from '../../Utility/localStorage';
 import MarkedBook from '../MarkedBook/MarkedBook';
-import { getWishlist } from '../../Utility/wishlist';
 import WishBook from '../WishBook/WishBook';
-// import { useState } from 'react';
+
 
 const ListedBooks = () => {
-    const books = useLoaderData();
-    // console.log(books);
-
-    const idList = getStoredBooks();
-    const idWishlist = getWishlist();
-
-    const readList = [];
-    const wishlist = [];
-
-    for (const id of idList) {
-        const dataOfReadList = books.find(book => book.bookId === id);
-        readList.push(dataOfReadList);
-    }
     
-    for (const id of idWishlist) {
-        const dataOfReadList = books.find(book => book.bookId === id);
-        wishlist.push(dataOfReadList);
-    }
-    const filteredWishlist = wishlist.filter((book) => !readList.some((readBook) => readBook.bookId === book.bookId));
-
-    // console.log(readList);
-    // console.log(wishlist);
-    console.log(filteredWishlist);
 
     return (
         <div>
@@ -39,7 +15,7 @@ const ListedBooks = () => {
                 <summary className="font-semibold text-xl text-[#FFFFFF] bg-[#23BE0A] rounded-lg px-4 py-2 w-32 mx-auto my-1">Sort By</summary>
                 <div className="flex justify-center">
                     <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-48">
-                        <li><a>Rating</a></li>
+                        <li onClick={ratingHandler}><a>Rating</a></li>
                         <li><a>Number of pages</a></li>
                         <li><a>Published year</a></li>
                     </ul>
@@ -61,7 +37,7 @@ const ListedBooks = () => {
                 <TabPanel>
                     <div>
                         {
-                            filteredWishlist.map(markedBook => <WishBook key={markedBook.bookId} markedBook={markedBook}></WishBook>)
+                            wishlist.map(markedBook => <WishBook key={markedBook.bookId} markedBook={markedBook}></WishBook>)
                         }
                     </div>
                 </TabPanel>
